8d2d0675add0d6b94328755a18bcfbfc25b735eb
joseglzc <93393952+joseglzc@users.noreply.github.com>
2022-01-31 19:32:13 +0100

    Add files via upload
    Ahora si que funciona, resubido

---
+++ multi-languaje-ui/src/main/java/es/ideas/MultiLenguajeUI.java
@@ -0,0 +1,50 @@
+package es.ideas;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * JavaFX App
+ */
+public class MultiLenguajeUI extends Application {
+
+    private static Stage primaryStage;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        
+        MultiLenguajeUI.primaryStage = stage;
+    
+        //Cargamos la app con el fxml indicado y el idioma indicado.
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("primary.fxml"));
+
+        //Seleccionamos el idioma predeterminado
+        loader.setResources(ResourceBundle.getBundle("es.ideas.i18n/cadenas",
+                Locale.getDefault()));
+        Parent raiz = loader.load();
+        //PrimaryController pc = loader.getController();
+        //pc.setMainWindow(this);
+
+        Scene scene = new Scene(raiz);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+      public static Stage getPrimaryStage(){
+        return primaryStage;
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
